3049a94b530ff4f35d4269b96409121911709730
RoryEiffe <roryeiffe8@gmail.com>
2022-07-27 16:08:32 -0400

    first commit

---
+++ src/main/java/com/revature/azuredemo7272022/AzureDemo7272022Application.java
@@ -0,0 +1,13 @@
+package com.revature.azuredemo7272022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDemo7272022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDemo7272022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/azuredemo7272022/AzureDemo7272022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.azuredemo7272022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDemo7272022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
